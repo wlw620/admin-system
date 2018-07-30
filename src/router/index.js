7f69f021c782c6342484eba027bff0ec1124960a
毋乐玮 <yuewei.wu@corp.elong.com>
2018-07-30 17:17:05 +0800

    feat: update

---
+++ src/router/index.js
@@ -0,0 +1,14 @@
+import Vue from 'vue';
+import Router from 'vue-router';
+
+import Dashboard from '@/pages/dashboard/Dashboard';
+
+Vue.use(Router);
+
+export default new Router({
+  routes: [{
+    path: '/',
+    name: 'Dashboard',
+    component: Dashboard,
+  }],
+});
